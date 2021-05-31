dd49911ab04a1ceb86bca6d4541aece255e3ae02
Daniel Washington <danielvertigo@hotmail.com>
2021-05-31 13:52:25 -0300

    Files

--- FilterbyDigitLength/ViewController.swift
+++ FilterbyDigitLength/ViewController.swift
@@ -5,6 +5,30 @@
 //  Created by Daniel Washington Ignacio on 30/05/21.
 //
 
+
+/*
+ Create a function that filters out an array to include numbers that only have a certain number of digits.
+
+ Examples
+
+ filterDigitLength([88, 232, 4, 9721, 555], 3) ➞ [232, 555]
+ // Include only numbers with 3 digits.
+
+ filterDigitLength([2, 7, 8, 9, 1012], 1) ➞ [2, 7, 8, 9]
+ // Include only numbers with 1 digit.
+
+ filterDigitLength([32, 88, 74, 91, 300, 4050], 1) ➞ []
+ // No numbers with only 1 digit exist => return empty array.
+
+ filterDigitLength([5, 6, 8, 9], 1) ➞ [5, 6, 8, 9]
+ // All numbers in the array have 1 digit only => return original array.
+ Notes
+
+ If no numbers of the specified digit length exist, return an empty array.
+ If all numbers in the array have the specified digit length, return the original array.
+ The sub-array returned should have the same relative order as the original array.
+ */
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,8 +36,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print(filterDigitLength([88, 232, 4, 9721, 555], 3))
     }
 
+    func filterDigitLength(_ arr: [Int], _ num: Int) -> [Int] {
+        var newArr: [Int] = []
+        for i in 0..<arr.count {
+                var number = arr[i]
+                var count = 0
+
+                while number > 0 {
+                    number = number/10
+                    count = count + 1
+                }
+
+                if count == num {
+                    newArr.append(arr[i])
+                }
+            }
+
+            return newArr
 
 }
 
+}
